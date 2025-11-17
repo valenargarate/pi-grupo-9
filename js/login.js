f6f51084ae3c8449a05a4a88baa52d832ed5b4b5
bergallolucia <bergallol@udesa.edu.ar>
2025-11-17 13:38:21 -0300

    hice el  javascript del login

---
+++ js/login.js
@@ -0,0 +1,41 @@
+let form = document.querySelector("#loginForm"); 
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+
+    let email = this.email.value;
+    let password = this.password.value; 
+
+    let mensajeDeError = document.querySelector("#loginError"); 
+    mensajeDeError.innerText = ""; 
+
+    if (email === "") {
+        mensajeDeError.innerText = ("El email es obligatorio."); 
+        alert("El email es obligatorio.");
+        return; 
+    }
+
+    if (password === "") {
+        mensajeDeError.innerText = ("La contraseña es obligatoria."); 
+        alert("La contraseña es obligatoria."); 
+        return; 
+    }
+
+    if (password.length < 6){
+        mensajeDeError.innerText = ("La contraseña debe tener como minimo 6 caracteres!");
+        alert("La contraseña debe tener como minimo 6 caracteres!");
+        return;
+    }
+
+  let userObjeto = {
+    email: email, 
+    password: password
+  }; 
+
+  let userString = JSON.stringify(userObjeto);
+
+  localStorage.setItem("userData", userString);
+
+  this.submit();
+  
+});
