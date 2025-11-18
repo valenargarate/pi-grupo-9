8c00eb90315afebadd4e58e41879cde8800c42ed
Fellynch <flynch@udesa.edu.ar>
2025-11-18 17:12:21 -0300

    hice el js de Category y arregle su HTML

---
+++ js/category.js
@@ -0,0 +1,55 @@
+const queryString = location.search;
+const queryStringObj = new URLSearchParams(queryString);
+
+let categoriaId = queryStringObj.get('id');
+
+if (categoriaId === null) { 
+    categoriaId = 'beauty';
+}
+
+const tituloCategoria = document.querySelector('#categoryTitle');
+const listaProductos = document.querySelector('.velasIndex1');
+
+if (tituloCategoria !== null) {
+    tituloCategoria.innerText = 'Categoría: ' + categoriaId;
+}
+
+const url = `https://dummyjson.com/products/category/${categoriaId}`;
+
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        const productos = data.products;
+
+        listaProductos.innerHTML = '';
+
+        let contenido = '';
+
+        let i = 0;                
+        while (i < productos.length) {   
+
+            const producto = productos[i];   
+
+            const detalleHref = './product.html?id=' + producto.id;
+
+            contenido = contenido +
+                '<li class="velas">' +
+                    '<img src="' + producto.thumbnail + '" alt="' + producto.title + '">' +
+                    '<h4>' + producto.title + '</h4>' +
+                    '<p>' + producto.description + '</p>' +
+                    '<div>$ ' + producto.price + '</div>' +
+                    '<a href="' + detalleHref + '">Ver detalle</a>' +
+                '</li>';
+
+            i++;   
+        }
+
+        listaProductos.innerHTML = contenido;
+    })
+    .catch(function (error) {
+        console.log('Error al cargar productos: ', error);
+        listaProductos.innerHTML = '<li>No se pudieron cargar los productos de esta categoría.</li>';
+    });
+
