c7c696bfe7a26e04b9a6b7959ac0531234edfe21
valenargarate <vargaratemena@udesa.edu.ar>
2025-11-21 08:39:06 -0300

    register guardar en local storage

--- js/register.js
+++ js/register.js
@@ -1,39 +1,47 @@
 let form = document.querySelector(".formIngreso");
 
-form.onsubmit = function () {
+form.onsubmit = function (e) {
+    e.preventDefault();  
 
     let email = form.email.value;
     let password = form.password.value;
     let password2 = form.password2.value;
     let terminos = form.terminos.checked;
 
-    
     if (email === "") {
         alert("El email es obligatorio.");
-        return false;
+        return;
     }
 
     if (password === "") {
         alert("La contraseña es obligatoria.");
-        return false;
+        return;
     }
 
     if (password.length < 6) {
         alert("La contraseña debe tener al menos 6 caracteres.");
-        return false;
+        return;
     }
 
     if (password !== password2) {
         alert("Las contraseñas no coinciden.");
-        return false;
+        return;
     }
 
     if (!terminos) {
         alert("Debes aceptar los términos y condiciones.");
-        return false;
+        return;
     }
 
+    
+    let userObjeto = {
+        email: email,
+        password: password
+    };
+
+    let userString = JSON.stringify(userObjeto);
+    localStorage.setItem("userData", userString);
+
     alert("Registro exitoso. Ahora podés iniciar sesión.");
-    location.href = "./index.html";
-    return false;
+    location.href = "./login.html";
 };
